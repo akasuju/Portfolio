e1383d005b04c9ebe5919f6272b808cd454cf54f
Sujan khatri <sujan.chettri1321@gmail.com>
2025-03-18 20:12:05 +0545

    add react-router-dom and create Nav component; refactor App component

---
+++ Portfolio/src/Pages/Nav.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+        <h1>Sujan</h1>
+        <div className="h-[10vh] w-[100vw] bg-[#222831] text-white justify-around item center">
+            <ul>
+                <li><a href="Home"></a></li>
+                <li><a href="About Me"></a></li>
+                <li><a href="Contact"></a></li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Nav
