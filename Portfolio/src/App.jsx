4287c20171192736f136e361988e09d522382a36
Sujan khatri <sujan.chettri1321@gmail.com>
2025-03-19 13:35:46 +0545

    refactor: clean up component imports and improve Footer social media icons

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,25 +1,22 @@
-import React from 'react'
-import Nav from './Pages/Nav.jsx'
-import HomePage from './Pages/HomePage.jsx'
-import AboutMe from './Pages/AboutMe.jsx'
-import ProjectPage from './Pages/ProjectPage.jsx'
-import ContactPage from './Pages/ContactPage.jsx'
-import Footer from './Pages/Footer.jsx'
-
-
+import React from "react";
+import Nav from "./Pages/Nav.jsx";
+import HomePage from "./Pages/HomePage.jsx";
+import AboutMe from "./Pages/AboutMe.jsx";
+import ProjectPage from "./Pages/ProjectPage.jsx";
+import ContactPage from "./Pages/ContactPage.jsx";
+import Footer from "./Pages/Footer.jsx";
 
 const App = () => {
   return (
     <div>
- <Nav />
-  <HomePage />
-  <AboutMe />
-  <ProjectPage/>
-<ContactPage/>
-<Footer/>
-  
- </div>
-  )
-}
+      <Nav />
+      <HomePage />
+      <AboutMe />
+      <ProjectPage />
+      <ContactPage />
+      <Footer />
+    </div>
+  );
+};
 
-export default App
+export default App;
--- Portfolio/src/Pages/Footer.jsx
+++ Portfolio/src/Pages/Footer.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import facebook from "../assets/facebook.png";
+import instagram from "../assets/instagram.png";
+import twitter from "../assets/twitter.png";
+import youtube from "../assets/youtube.png";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 text-white py-6 mt-10 text-center">
+    <footer className="bg-gray-900 text-white py-6 text-center">
       <div className="flex justify-center space-x-6 mb-4">
         <a href="#" className="text-gray-400 hover:text-white">Home</a>
         <a href="#" className="text-gray-400 hover:text-white">About me</a>
@@ -10,16 +14,16 @@ const Footer = () => {
       </div>
       <div className="flex justify-center space-x-4 mb-4">
         <a href="#" className="w-8 h-8 flex items-center justify-center bg-gray-700 rounded-full text-white">
-          F
+          <img src={facebook} alt="" />
         </a>
         <a href="#" className="w-8 h-8 flex items-center justify-center bg-gray-700 rounded-full text-white">
-          I
+        <img src={instagram} alt="" />
         </a>
         <a href="#" className="w-8 h-8 flex items-center justify-center bg-gray-700 rounded-full text-white">
-          T
+        <img src={twitter} alt="" />
         </a>
         <a href="#" className="w-8 h-8 flex items-center justify-center bg-gray-700 rounded-full text-white">
-          Y
+        <img src={youtube} alt="" />
         </a>
       </div>
       <p className="text-gray-500 text-sm">Terms of Service - Privacy Policy</p>
